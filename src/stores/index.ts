b1d15746d46e830e14aab3b6ae97b8fad6ce1f54
derpierre65 <git@derpierre65.de>
2024-06-06 20:53:20 +0100

    chore: initial commit

---
+++ src/stores/index.ts
@@ -0,0 +1,18 @@
+import { store } from 'quasar/wrappers';
+import { createPinia } from 'pinia';
+import { Router } from 'vue-router';
+
+declare module 'pinia' {
+  export type PiniaCustomProperties = {
+    readonly router: Router;
+  };
+}
+
+export default store((/* { ssrContext } */) => {
+  const pinia = createPinia();
+
+  // You can add Pinia plugins here
+  // pinia.use(SomePiniaPlugin)
+
+  return pinia;
+});
