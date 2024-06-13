5ccbf269ed2a8b4d732ac52ce614d9a463a5eac6
derpierre65 <git@derpierre65.de>
2024-06-13 19:35:55 +0100

    feat: add track name, auto save previous laps after track change

--- src/pages/TimeTrialPage.vue
+++ src/pages/TimeTrialPage.vue
@@ -14,6 +14,9 @@
       <div>
         <q-card class="tw-sticky tw-top-28" flat>
           <q-card-section>
+            <div v-if="currentTrackId">
+              {{ t('time_trial.stats.track_name') }}: <strong>{{ trackNames[currentTrackId] }}</strong>
+            </div>
             {{ t('time_trial.stats.best_lap_time') }}: <strong>{{ formatTimestamp(bestLapTime) }}</strong><br>
             {{ t('time_trial.stats.average_time') }}: <strong>{{ formatTimestamp(averageLapTime) }}</strong><br>
             {{ t('time_trial.stats.possible_best_time') }}: <strong>{{ formatTimestamp(bestSector1 + bestSector2 + bestSector3) }}</strong>
@@ -40,7 +43,7 @@
 import { computed, ref } from 'vue';
 import { type QTableProps, useQuasar } from 'quasar';
 import { formatTimestamp } from 'src/utils/type-helpers/math';
-import type { LapHistoryData, PacketSessionHistoryData } from '@racehub-io/f1-telemetry-client/build/main/parsers/packets/types';
+import type { LapHistoryData, PacketSessionData, PacketSessionHistoryData } from '@racehub-io/f1-telemetry-client/build/main/parsers/packets/types';
 import { useTranslation } from 'i18next-vue';
 import { useTelemetryClient } from 'src/composables/telemetry-client';
 import { PACKETS } from '@racehub-io/f1-telemetry-client/build/main/constants';
@@ -53,10 +56,46 @@ const q = useQuasar();
 const { t, } = useTranslation();
 useTelemetryClient({
   [PACKETS.sessionHistory]: onSessionHistory,
+  [PACKETS.session]: onSession,
 });
 //#endregion
 
 //#region Data
+const trackNames: Record<number, string> = {
+  0: 'Melbourne',
+  1: 'Paul Ricard',
+  2: 'Shanghai',
+  3: 'Sakhir (Bahrain)',
+  4: 'Catalunya',
+  5: 'Monaco',
+  6: 'Montreal',
+  7: 'Silverstone',
+  8: 'Hockenheim',
+  9: 'Hungaroring',
+  10: 'Spa',
+  11: 'Monza',
+  12: 'Singapore',
+  13: 'Suzuka',
+  14: 'Abu Dhabi',
+  15: 'Texas',
+  16: 'Brazil',
+  17: 'Austria',
+  18: 'Sochi',
+  19: 'Mexico',
+  20: 'Baku (Azerbaijan)',
+  21: 'Sakhir Short',
+  22: 'Silverstone Short',
+  23: 'Texas Short',
+  24: 'Suzuka Short',
+  25: 'Hanoi',
+  26: 'Zandvoort',
+  27: 'Imola',
+  28: 'Portimão',
+  29: 'Jeddah',
+  30: 'Miami',
+  31: 'Las Vegas',
+  32: 'Losail',
+};
 const requiredKeys = [
   'm_sector1TimeInMS',
   'm_sector2TimeInMS',
@@ -71,6 +110,7 @@ const rowsPerPageOptions = ref([
   50,
   100,
 ]);
+const currentTrackId = ref<number | null>(null);
 //#endregion
 
 //#region Computed
@@ -158,6 +198,15 @@ function onSessionHistory(data: PacketSessionHistoryData) {
   console.log('onSessionHistory', data);
 }
 
+function onSession(data: PacketSessionData) {
+  if (currentTrackId.value && currentTrackId.value !== data.m_trackId) {
+    save();
+    reset();
+  }
+
+  currentTrackId.value = data.m_trackId;
+}
+
 function getBestTime(key: keyof LapHistoryData) {
   return (previous: number, lap: LapHistoryData): number => {
     if (lap[key] === 0) {
@@ -212,14 +261,29 @@ function loadFile(event) {
   reader.readAsText(event.target.files[0]);
 }
 function save() {
+  if (!currentTrackId.value) {
+    return;
+  }
+
   const link = document.createElement('a');
   const file = new Blob([ JSON.stringify(laps.value), ], {
     type: 'text/plain',
   });
   link.href = URL.createObjectURL(file);
-  link.download = 'time_trial.txt'; // TODO add map name
+
+  const trackName = trackNames[currentTrackId.value]
+    .toLowerCase()
+    .replace(/\s+\(\s*/, '_')
+    .replace(/\s+\)/, '')
+    .replace(/\W/g, '');
+
+  link.download = `time_trial_${currentTrackId.value}_${trackName}.txt`;
   link.click();
   URL.revokeObjectURL(link.href);
+}
+
+function reset() {
+  laps.value = [];
 }
 //#endregion
 
